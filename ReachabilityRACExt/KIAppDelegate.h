f4173f8c5b9220c8618022c5183482fcfe19306f
Kai INUI <lied.der.optik@gmail.com>
2014-08-19 15:46:39 +0900

    initial

---
+++ ReachabilityRACExt/KIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KIAppDelegate.h
+//  ReachabilityRACExt
+//
+//  Created by kaiinui on 2014/08/19.
+//  Copyright (c) 2014年 kaiinui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReachabilityRACExt/KIViewController.h
@@ -0,0 +1,13 @@
+//
+//  KIViewController.h
+//  ReachabilityRACExt
+//
+//  Created by kaiinui on 2014/08/19.
+//  Copyright (c) 2014年 kaiinui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KIViewController : UIViewController
+
+@end
